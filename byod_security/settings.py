56803cef697034ea09ac7869ad38f53032bbe30a
Ankitkr95 <ankitkrmahto@gmail.com>
2025-11-15 22:44:21 +0530

    deploy access error

--- byod_security/settings.py
+++ byod_security/settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-6b&xon$kf$2e*%(x%2)@s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'byod-o0g9.onrender.com',
+]
 
 
 # Application definition
